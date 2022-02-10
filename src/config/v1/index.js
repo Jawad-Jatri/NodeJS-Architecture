d3d18d52dbad33dc6f78aaef6a85b77b79dc401f
akiljawad976 <jawad.jatri@gmail.com>
2022-02-11 00:23:27 +0600

    init user done

--- src/config/v1/index.js
+++ src/config/v1/index.js
@@ -1,13 +1,13 @@
 const {Routes} = require("../../api/v1/routes")
-const {StatusCodes} = require('../../api/v1/utils/status-codes');
-const {ResponseMessage} = require('../../api/v1/utils/response-message');
 const {errorHandlerMiddleware} = require("../../api/v1/middlewares/error-handler")
 const {notFound} = require("../../api/v1/middlewares/not-found")
-const Path = require("../../api/v1/utils/api-urls")
-const Bcrypt = require("../../api/v1/utils/bcrypt")
-const {successCallback} = require("../../api/v1/success-callback")
-const CustomError = require("../../api/v1/errors")
 const {validator} = require("../../api/v1/validations")
+const {successCallback} = require("../../api/v1/success-callback")
+const {StatusCodes} = require('../../api/v1/utils/status-codes');
+const {ResponseMessage} = require('../../api/v1/utils/response-message');
+const Path = require("../../api/v1/utils/api-urls")
+const CustomError = require("../../api/v1/errors")
+const Bcrypt = require("../../api/v1/utils/bcrypt")
 
 const v1 = (application) => {
   const {app} = application
